e4a5e6e2ad638947d9412472e7d7d9dbb17f04ba
François <francois.novais@gmail.com>
2025-12-04 15:18:19 +0100

    feat(books): add getBookById route and controller with Rest client test file

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -1,7 +1,21 @@
 import type { Request, Response } from "express";
 import { prisma } from "../models/index.ts";
+import { parseIdFromParams } from "./utils.ts";
+import z from "zod";
 
 export async function getAllBooks(req: Request, res: Response) {
   const books = await prisma.books.findMany();
   res.json(books);
+}
+
+export async function getBookById(req: Request, res: Response) {
+  const bookId = await parseIdFromParams(req.params.id);
+  const book = await prisma.books.findUnique({
+    where: { id: bookId },
+  });
+
+  if (! book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  res.json(book);
 }
--- src/routers/books.router.ts
+++ src/routers/books.router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { getAllBooks } from "../controllers/books.controller.ts";
+import { getAllBooks, getBookById } from "../controllers/books.controller.ts";
 
 export const router = Router();
 
 router.get("/books", getAllBooks); 
+
+router.get("/books/:id", getBookById);
