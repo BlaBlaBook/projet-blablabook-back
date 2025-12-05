9f3e6ae8bd67c578b6e1bcb9456ba7b92c6376c6
François <francois.novais@gmail.com>
2025-12-05 14:39:55 +0100

    feat(api-books): add author and genre to getAllBooks response

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -5,9 +5,46 @@ import { ConflictError, NotFoundError } from "../lib/error.ts";
 import { createBookSchema, updateBookSchema } from "../schemas/books.schema.ts";
 
 export async function getAllBooks(req: Request, res: Response) {
-  const books = await prisma.books.findMany();
-  res.json(books);
+  const books = await prisma.books.findMany({
+    include: {
+      authors: {
+        include: {
+          author: true,
+        },
+      },
+      genres: {
+        include: {
+          genre: true,
+        },
+      },
+    },
+  });
+
+  const formattedBooks = books.map((b) => ({
+    id: b.id,
+    isbn: b.isbn,
+    title: b.title,
+    year: b.year,
+    summary: b.summary,
+    language: b.language,
+    pages: b.pages,
+    image_url: b.image_url,
+    authors: b.authors.map((ba) => ({
+      id: ba.author.id,
+      first_name: ba.author.first_name,
+      last_name: ba.author.last_name,
+    })),
+    genres: b.genres.map((bg) => ({
+      id: bg.genre.id,
+      category: bg.genre.category,
+    })),
+    created_at: b.created_at,
+    updated_at: b.updated_at,
+  }));
+
+  res.json(formattedBooks);
 }
+
 
 export async function getBookById(req: Request, res: Response) {
   const bookId = await parseIdFromParams(req.params.id);
