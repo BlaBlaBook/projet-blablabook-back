21f0a2b21e77c94a406038d4a6d231be401e0cbd
MaximePAPA <maxime.papa@oclock.school>
2025-12-08 09:38:52 +0100

    add: migration-prisma auth-middleware auth-lib

---
+++ src/middlewares/ensure-auth-.middleware.ts
@@ -0,0 +1,39 @@
+import type { NextFunction, Request, Response } from "express";
+import { extractAccessTokenFromRequest, decodeJWT } from "../lib/token.ts";
+import { attemptRefresh } from "../lib/auth.ts";
+
+export async function ensureAuthenticated(error: Error,  req: Request, res: Response, next: NextFunction) {
+  try {
+    // Try access token
+    const token = extractAccessTokenFromRequest(req);
+    const { userId, userRole } = decodeJWT(token);
+
+    req.userId = userId;
+    req.userRole = userRole;
+    return next();
+
+  } catch (error) {
+    // Type guard pour error
+    const errorMessage = error instanceof Error ? error.message : String(error);
+    
+    // AccessToken expired → try refresh
+    if (!errorMessage.includes("expired")) {
+      // Other JWT error → invalid token
+      return next(error);
+    }
+
+    // Attempt silent refresh
+    try {
+      const { accessToken, user } = await attemptRefresh(req, res);
+
+      const { userId, userRole } = decodeJWT(accessToken);
+
+      req.userId = userId;
+      req.userRole = userRole;
+      return next();
+
+    } catch (refreshErr) {
+      return next(refreshErr);
+    }
+  }
+}
