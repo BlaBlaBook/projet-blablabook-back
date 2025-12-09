abd59de45a74d9f89fefabc078704a1281bc9e9f
JordanDonguy <jordan.donguy@gmail.com>
2025-12-09 10:05:27 +0100

    refactor: simplify login and logout routes by removing unnecessary role checks

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import { getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, deleteCurrentUser, updateCurrentUser } from "../controllers/auth.controller.ts"
 import {  isAuth } from "../middlewares/isAuth.middleware.ts";
-import { allowRoles } from "../middlewares/allow-roles.middleware.ts";
 
 export const router = Router();
 
 router.post("/auth/register", registerUser);
-router.post("/auth/login", isAuth, loginUser);
+router.post("/auth/login", loginUser);
 router.post("/auth/refresh", refreshAccessToken);
-router.post("/auth/logout", isAuth, allowRoles(["user"]), logoutUser);
-router.get("/auth/me", isAuth, allowRoles(["user", "admin"]), getCurrentUser);
-router.delete("/auth/me", isAuth, allowRoles(["user", "admin"]), deleteCurrentUser);
-router.patch("/auth/me", isAuth, allowRoles(["user", "admin"]), updateCurrentUser);
+router.post("/auth/logout", isAuth, logoutUser);
+router.get("/auth/me", isAuth, getCurrentUser);
+router.delete("/auth/me", isAuth, deleteCurrentUser);
+router.patch("/auth/me", isAuth, updateCurrentUser);
