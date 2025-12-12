07ec8e93a0a6242e729eee1efe89539d7f4c492c
JordanDonguy <jordan.donguy@gmail.com>
2025-12-12 16:15:00 +0100

    fix(auth): check currentPassword before updating password, delete refreshToken routes (not used, managed in middlewares)

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import { getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, deleteCurrentUser, updateCurrentUser } from "../controllers/auth.controller.ts"
+import { getCurrentUser, loginUser, logoutUser, registerUser, deleteCurrentUser, updateCurrentUser } from "../controllers/auth.controller.ts"
 import { isAuth } from "../middlewares/isAuth.middleware.ts";
 
 export const router = Router();
 
 router.post("/auth/register", registerUser);
 router.post("/auth/login", loginUser);
-router.post("/auth/refresh", refreshAccessToken);
 router.post("/auth/logout", isAuth, logoutUser);
 router.get("/auth/me", isAuth, getCurrentUser);
 router.delete("/auth/me", isAuth, deleteCurrentUser);
