378467365edee2e07c11ed4d5d2ca239f6635451
JordanDonguy <jordan.donguy@gmail.com>
2025-12-15 14:15:18 +0100

    feat(auth): implement google OAuth with a new route and controller, add create password feature in update user profile route (patch /auth/me)

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getCurrentUser, loginUser, logoutUser, registerUser, deleteCurrentUser, updateCurrentUser, regenerateAvatar } from "../controllers/auth.controller.ts"
+import { getCurrentUser, loginUser, logoutUser, registerUser, deleteCurrentUser, updateCurrentUser, googleAuth, regenerateAvatar } from "../controllers/auth.controller.ts"
 import { isAuth } from "../middlewares/isAuth.middleware.ts";
 
 export const router = Router();
 
 router.post("/auth/register", registerUser);
 router.post("/auth/login", loginUser);
+router.post("/auth/google", googleAuth);
 router.post("/auth/logout", isAuth, logoutUser);
 router.get("/auth/me", isAuth, getCurrentUser);
 router.delete("/auth/me", isAuth, deleteCurrentUser);
