2e89f5e8431438e2762bf7b4e6b5b054c99804bb
JordanDonguy <jordan.donguy@gmail.com>
2025-12-19 13:48:48 +0100

    feat: add welcoming message at "/" api route

--- src/app.ts
+++ src/app.ts
@@ -5,31 +5,43 @@ import cors from "cors";
 import { config } from "../config.ts";
 import { router as apiRouter } from "./routers/index.router.ts";
 import { globalErrorHandler } from "./middlewares/global-error-handler.middleware.ts";
-import swaggerUi from 'swagger-ui-express';
-import { specs } from './swagger.config.ts';
+import swaggerUi from "swagger-ui-express";
+import { specs } from "./swagger.config.ts";
+import { welcomingMsg } from "./lib/html.ts";
 
 // Create Express app
 export const app = express();
 
 // Autoriser les requêtes cross-origin
-app.use(cors({ 
-  origin: config.allowedOrigins,
-  credentials: true,
-}));
+app.use(
+	cors({
+		origin: config.allowedOrigins,
+		credentials: true,
+	}),
+);
 
 // Body parser (application/json)
 app.use(express.json());
 
-// Cookie parser 
+// Cookie parser
 app.use(cookieParser());
 
 // Configuration
 app.use("/api", apiRouter);
 
 // Swagger documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, {
-  customSiteTitle: 'Blablabook API Documentation',
-}));
+app.use(
+	"/api-docs",
+	swaggerUi.serve,
+	swaggerUi.setup(specs, {
+		customSiteTitle: "Blablabook API Documentation",
+	}),
+);
+
+// Welcoming message
+app.get("/", (req, res) => {
+	res.send(welcomingMsg);
+});
 
 // Global error middleware
 app.use(globalErrorHandler);
